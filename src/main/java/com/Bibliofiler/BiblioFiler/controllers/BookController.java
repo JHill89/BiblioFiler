fb083a4b232f4a7d9ea5208f956df833fe9732c7
Joni Hill <joni.hill@rocketmail.com>
2020-03-26 19:00:08 -0500

    at /books/create page on submit nav to home and displays title and author

--- src/main/java/com/Bibliofiler/BiblioFiler/controllers/BookController.java
+++ src/main/java/com/Bibliofiler/BiblioFiler/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.Bibliofiler.BiblioFiler.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
--- src/main/java/com/Bibliofiler/BiblioFiler/controllers/HomeController.java
+++ src/main/java/com/Bibliofiler/BiblioFiler/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package com.Bibliofiler.BiblioFiler.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String home(){
+    @RequestMapping(value = "index", method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(@RequestParam String title, String author, String notes, Model model){
+        String byAuthor = "by " + author;
+        model.addAttribute("title", title);
+        model.addAttribute("byAuthor", byAuthor);
+        model.addAttribute("notes", notes);
         return "index";
     }
 }
