2b5c241633548439dc81f721e6acff844341ae88
Joni Hill <joni.hill@rocketmail.com>
2020-04-01 14:49:42 -0500

    added 'All Books' link on home screen, created allBooks page

--- src/main/java/com/Bibliofiler/BiblioFiler/controllers/BookController.java
+++ src/main/java/com/Bibliofiler/BiblioFiler/controllers/BookController.java
@@ -5,8 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BookController {
+
+    @GetMapping("books/allBooks")
+    public String allBooksPage(Model model) {
+        List<String> allBooks = new ArrayList<>();
+        allBooks.add("Year One");
+        allBooks.add("The Rise of Magicks");
+        allBooks.add("Harry Potter and the Sorcerers Stone");
+        allBooks.add("Lunatic Cafe");
+        allBooks.add("The Horse and His Boy");
+        allBooks.add("The Solar War");
+        model.addAttribute("allBooks", allBooks);
+        return "books/allBooks";
+    }
 
     @GetMapping("books/create")
     public String createNewBook() {
--- src/main/java/com/Bibliofiler/BiblioFiler/controllers/HomeController.java
+++ src/main/java/com/Bibliofiler/BiblioFiler/controllers/HomeController.java
@@ -15,5 +15,6 @@ public class HomeController {
         model.addAttribute("notes", notes);
         return "index";
     }
+
 }
 
